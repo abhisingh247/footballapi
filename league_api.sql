7058a517530fb5f1383f23f334458ea71e892384
gareth <gareth@joannalark.com>
2018-06-22 21:44:19 +0530

    thrid commit

---
+++ league_api.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 22, 2018 at 09:16 PM
+-- Server version: 5.7.22-0ubuntu0.16.04.1
+-- PHP Version: 7.2.6-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `league_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `football_team`
+--
+
+CREATE TABLE `football_team` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `strip` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `football_team`
+--
+
+INSERT INTO `football_team` (`id`, `name`, `strip`) VALUES
+(1, 'N Team', 1),
+(3, 'KM Team', 1),
+(5, 'Mask Team', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `league`
+--
+
+CREATE TABLE `league` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `league`
+--
+
+INSERT INTO `league` (`id`, `name`) VALUES
+(1, 'League 1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `token` varchar(60) DEFAULT NULL,
+  `token_expire` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `token`, `token_expire`) VALUES
+(1, 'abhi', 'd76f3d05cc9ac98f1f9160274a39fe33', 'abhi', 'b5473c320f6d1fe5', '2018-06-22 20:57:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `football_team`
+--
+ALTER TABLE `football_team`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `strip` (`strip`);
+
+--
+-- Indexes for table `league`
+--
+ALTER TABLE `league`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `football_team`
+--
+ALTER TABLE `football_team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `league`
+--
+ALTER TABLE `league`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `football_team`
+--
+ALTER TABLE `football_team`
+  ADD CONSTRAINT `football_team_ibfk_1` FOREIGN KEY (`strip`) REFERENCES `league` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
